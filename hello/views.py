e63137b9d1764d5f4a5b07983d560b411763dd8e
Alexandre Gaspard <alexgaspard@gmail.com>
2018-10-26 15:42:19 +0000

    [add] Adds sqlite db and test function

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from sqlite3 import connect, Error
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,17 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def test(request):
+    try:
+        con = lite.connect('us-census.db')
+        cur = con.cursor()    
+        cur.execute('SELECT SQLITE_VERSION()')
+        data = cur.fetchone()
+        print "SQLite version: %s" % data
+    except lite.Error, e:
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
