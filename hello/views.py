f4ca762fad0853ba54b4ebf2788b1dd262a82f92
Alexandre Gaspard <alexgaspard@gmail.com>
2018-10-26 16:13:47 +0000

    [test] Test sqlite db location

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotAllowed, HttpRequest, JsonR
 
 from .models import Greeting
 from sqlite3 import connect, Error
+from os import getcwd
 
 # Create your views here.
 def index(request):
@@ -21,16 +22,16 @@ def db(request):
 
 def test(request):
     try:
-        con = connect('us-census.db')
+        con = connect('hello/us-census.db')
         cur = con.cursor()    
         cur.execute('SELECT SQLITE_VERSION()')
         version = cur.fetchone()
         
-        cur.execute('select education, count(*) as c, avg(age), (select count(distinct education) from census_learn_sql) as e from census_learn_sql group by education order by education desc limit 100')
-        data = cur.fetchall()
+        #cur.execute('select education, count(*) as c, avg(age), (select count(distinct education) from census_learn_sql) as e from census_learn_sql group by education order by education desc limit 100')
+        data = '' #cur.fetchall()
 
         #select * from census_learn_sql where rowid=215094;
-        return JsonResponse({'version':'SQLite version: %s'%version,'data':data})
+        return JsonResponse({'dir':getcwd(),'version':'SQLite version: %s'%version,'data':data})
     except Error as e:
         return JsonResponse({'error':'%s'%e.args[0]})
     finally:
