22aa878b127ab35421f46e2c62c35496a2ba9126
Alexandre Gaspard <alexgaspard@gmail.com>
2018-10-31 16:46:22 -0100

    [add] Adds e2e template files and updates libs and adds test on title

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,10 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { StatisticsModule } from './statistics';
 
 
@@ -15,12 +14,12 @@ import { StatisticsModule } from './statistics';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    FormsModule,
+    // ReactiveFormsModule,
+    // FormsModule,
     HttpClientModule,
     AppRoutingModule,
     StatisticsModule,
-    NgbModule.forRoot()
+    // NgbModule
   ],
   bootstrap: [AppComponent]
 })
