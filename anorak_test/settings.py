39e715d2f99a0f2aad71f8ea068aed9f292865d9
Alexandre Gaspard <alexgaspard@gmail.com>
2018-10-31 11:04:43 -0100

    [clean] Trusts collectstatic to get anular files from dist folder to staticfiles

--- anorak_test/settings.py
+++ anorak_test/settings.py
@@ -110,5 +110,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'front-end/dist'),
+)
 
 django_heroku.settings(locals())
