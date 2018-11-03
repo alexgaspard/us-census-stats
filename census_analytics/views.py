12f3f3dc95c144bd20b21fcadf402bb3664f1d15
Alexandre Gaspard <alexgaspard@gmail.com>
2018-11-03 20:00:33 +0100

    [add] Returns errors as JSON

--- census_analytics/views.py
+++ census_analytics/views.py
@@ -1,3 +1,5 @@
+from json import dumps
+
 from django.http import JsonResponse, HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
 from django.utils.html import escape
 
@@ -5,6 +7,8 @@ from census_analytics.adapters.sqlite import SQLite
 from census_analytics.exceptions.database_exception import DatabaseException
 from census_analytics.exceptions.field_not_allowed import FieldNotAllowed
 from census_analytics.managers.statistics_manager import StatisticsManager
+
+APPLICATION_JSON = 'application/json'
 
 db = SQLite('us-census.db')
 statistics_manager = StatisticsManager(db)
@@ -16,9 +20,10 @@ def stats(request: HttpRequest) -> HttpResponse:
     try:
         # TODO Use Marshmallow or other to validate input
         if 'field' not in request.GET:
-            return HttpResponseBadRequest('"field" is mandatory')
+            return HttpResponseBadRequest(dumps({'error': '"field" is mandatory'}), content_type=APPLICATION_JSON)
         if not isinstance(request.GET['field'], str):
-            return HttpResponseBadRequest('"field" should be a string')
+            return HttpResponseBadRequest(dumps({'error': '"field" should be a string'}),
+                                          content_type=APPLICATION_JSON)
         field = escape(request.GET['field'])
         statistics = statistics_manager.get_statistics(field)
         total = statistics_manager.get_total(field)
@@ -27,6 +32,6 @@ def stats(request: HttpRequest) -> HttpResponse:
             skipped_lines_count = statistics_manager.get_skipped_lines_count(field)
         return JsonResponse({'data': statistics, 'total': total, 'skipped_lines_count': skipped_lines_count})
     except FieldNotAllowed as e:
-        return HttpResponseBadRequest(e.get_message())
+        return HttpResponseBadRequest(dumps({'error': e.get_message()}), content_type=APPLICATION_JSON)
     except DatabaseException as e:
         return JsonResponse({'error': e.get_message()})
