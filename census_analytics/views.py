5e4ee59647b5f3cd2b49b09afeaa5bffa94dbee2
Alexandre Gaspard <alexgaspard@gmail.com>
2018-11-03 19:44:37 +0100

    [add] Truncates average age, hides table overflow, returns bad request if field not present

--- census_analytics/views.py
+++ census_analytics/views.py
@@ -3,6 +3,7 @@ from django.utils.html import escape
 
 from census_analytics.adapters.sqlite import SQLite
 from census_analytics.exceptions.database_exception import DatabaseException
+from census_analytics.exceptions.field_not_allowed import FieldNotAllowed
 from census_analytics.managers.statistics_manager import StatisticsManager
 
 db = SQLite('us-census.db')
@@ -13,14 +14,19 @@ def stats(request: HttpRequest) -> HttpResponse:
     if request.method not in ['GET']:
         return HttpResponseNotAllowed(['GET'])
     try:
-        field = escape(request.GET['field'])
-        if not isinstance(field, str):
+        # TODO Use Marshmallow or other to validate input
+        if 'field' not in request.GET:
+            return HttpResponseBadRequest('"field" is mandatory')
+        if not isinstance(request.GET['field'], str):
             return HttpResponseBadRequest('"field" should be a string')
+        field = escape(request.GET['field'])
         statistics = statistics_manager.get_statistics(field)
         total = statistics_manager.get_total(field)
         skipped_lines_count = 0
         if total > len(statistics):
             skipped_lines_count = statistics_manager.get_skipped_lines_count(field)
         return JsonResponse({'data': statistics, 'total': total, 'skipped_lines_count': skipped_lines_count})
+    except FieldNotAllowed as e:
+        return HttpResponseBadRequest(e.get_message())
     except DatabaseException as e:
         return JsonResponse({'error': e.get_message()})
